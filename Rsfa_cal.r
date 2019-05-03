faed196fd76617bfd40b87563c36f203234686dd
Mike Zhu <zhuyuan2706@gmail.com>
2019-05-03 22:37:25 +0800

    First stage rerun

--- Rsfa_cal.r
+++ Rsfa_cal.r
@@ -3,9 +3,9 @@ library(frontier)
 
 for (year in 2006:2016) {
   data1 <- read_excel(paste('RFrontierInputFiles/_sfa_in', year, '.xls', sep = ""))
-  sfa_data_co2 <- sfa( (-Slack_CO2) ~ Urbanization + Secondary_Industry + Capita_GDP + Coal_Consume + People_Density | -1, data = data1 )
-  sfa_data_capital <- sfa( (-Slack_CAPITAL) ~ Urbanization + Secondary_Industry + Capita_GDP + Coal_Consume + People_Density | -1, data = data1 )
-  sfa_data_work <- sfa( (-Slack_WORK) ~ Urbanization + Secondary_Industry + Capita_GDP + Coal_Consume + People_Density | -1, data = data1 )
+  sfa_data_co2 <- sfa( (-Slack_CO2) ~ Secondary_Industry + Car_Ownership | -1, data = data1 )
+  sfa_data_capital <- sfa( (-Slack_CAPITAL) ~ Secondary_Industry + Car_Ownership | -1, data = data1 )
+  sfa_data_work <- sfa( (-Slack_WORK) ~ Secondary_Industry + Car_Ownership | -1, data = data1 )
 
   sink(paste('RFrontierOutputFiles/_sfa_out_xx', year, '.txt', sep = ""))
   cat(paste('--------', year, '---------\n\n**CO2**\n\n', sep = ""))
@@ -29,9 +29,9 @@ cal_sfa_in<-function(){
 year <- 2016
 # for (year in 2006:2016) {
 data1 <- read_excel(paste('RFrontierInputFiles/_sfa_in', year, '.xls', sep = ""))
-sfa_data_co2 <- sfa( (-Slack_CO2) ~ Urbanization + Secondary_Industry + Capita_GDP + Coal_Consume + People_Density + Car_Ownership + Energy_Invest + Resource_Tax | -1, data = data1 )
-sfa_data_capital <- sfa( (-Slack_CAPITAL) ~ Urbanization + Secondary_Industry + Capita_GDP + Coal_Consume + People_Density + Car_Ownership + Energy_Invest + Resource_Tax | -1, data = data1 )
-sfa_data_work <- sfa( (-Slack_WORK) ~ Urbanization + Secondary_Industry + Capita_GDP + Coal_Consume + People_Density + Car_Ownership + Energy_Invest + Resource_Tax | -1, data = data1 )
+sfa_data_co2 <- sfa( (-Slack_CO2) ~ Secondary_Industry + Car_Ownership | -1, data = data1 )
+sfa_data_capital <- sfa( (-Slack_CAPITAL) ~ Secondary_Industry + Car_Ownership | -1, data = data1 )
+sfa_data_work <- sfa( (-Slack_WORK) ~ Secondary_Industry + Car_Ownership | -1, data = data1 )
 
 sink(paste('RFrontierOutputFiles/_sfa_out', year, '.txt', sep = ""))
 print(paste('********', year, '********\n\n**CO2**\n\n', sep = ""))
